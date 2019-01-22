32b1ef6b32d3b4c67018b8967ffc49c8a57122e6
Eddy <almuina.e@gmail.com>
2019-01-22 20:09:51 +0100

    batch tests

--- test.py
+++ test.py
@@ -8,13 +8,10 @@ from domain import DomainProblem
 from state import State
 
 
-def main(argv):
-    domainfile = "./examples-pddl/domains/%s.pddl" % argv[2]
-    problemfile = "./examples-pddl/problems/%s.pddl" % argv[3]
+def test(domain, problem, config):
+    domainfile = "./examples-pddl/domains/%s.pddl" % domain
+    problemfile = "./examples-pddl/problems/%s.pddl" % problem
     domprob = DomainProblem(domainfile, problemfile)
-
-    with open(argv[1]) as file:
-        config = json.load(file)
 
     initial = State(frozenset({tuple(atom.predicate) for atom in domprob.initialstate()}))
     goals = set(tuple(atom.predicate) for atom in domprob.goals())
@@ -31,9 +28,27 @@ def main(argv):
     for i, action in enumerate(map(lambda x: x[1], s)):
         print(f'{i + 1} \t {action.operator_name} {str(action.variable_list)}')
 
-    print()
+    print('--------------------------------------------------')
     print(f'IS GOAL:\t{s[-1][0].apply(s[-1][1]).is_goal(goals)}')
     print('TIME:\t\t%.2f s' % (time.time() - start))
+    print('--------------------------------------------------')
+
+
+def main(argv):
+    tests = [
+        ('domain-01', 'problem-01'),
+        ('domain-03', 'problem-03')
+    ]
+
+    with open(argv[1]) as file:
+        config = json.load(file)
+
+    for domain, problem in tests:
+        print('==================================================')
+        print(f'{domain} --- {problem}')
+        print('==================================================')
+        test(domain, problem, config)
+        print()
 
 
 if __name__ == '__main__':
